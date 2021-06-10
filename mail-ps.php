42863f81738eca08ab0e172b1c4f129912cfed04
Jyotirmay Dev <85521357+Jyotirmay-Dev@users.noreply.github.com>
2021-06-10 17:44:57 +0530

    Add files via upload

---
+++ mail-ps.php
@@ -0,0 +1,26 @@
+<?php
+
+//get data from form
+
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$to = "jyotirmay.dev2016@gmail.com";
+
+$subject = "Mail From Personal Website [JD]"
+$txt = "First Name = ".$firstname."\r\nLast Name = ".$lastname."\r\nEmail = ".$email."\r\nPhone = ".$phone."\r\nSubject = ".$subject."\r\nMessage = ".$message;
+
+$headers = "From: noreply@jyotirmaydev.com";
+
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+
+//redirect
+
+header("location:thankyou.html");
+?>
